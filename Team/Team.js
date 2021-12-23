f7cd54e526485b82d582509663143aba4da22c9e
Vishu-01 <p.rapolu@iitg.ac.in>
2021-12-23 19:30:19 +0530

    Added Team page

---
+++ Team/Team.js
@@ -0,0 +1,43 @@
+$(".carousel2").owlCarousel({
+    margin: 5,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
+
+$(".carousel3").owlCarousel({
+    margin: 5,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
